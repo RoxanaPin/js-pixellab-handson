4757bc2af7af275d086c65f02447837bab3ef36e
RoxanaPin <roxana.pintilie28@gmail.com>
2022-07-31 21:33:09 +0300

    tema 1

--- M01S04/ex04/app.js
+++ M01S04/ex04/app.js
@@ -30,8 +30,31 @@
 // }
 //nr divizibile cu 5
 
-for (var i = 1; i <= 100; i++) {
-  if (i % 5 !== 0) {
+// for (var i = 1; i < 90; i++) {
+//   console.log(i);
+// }
+
+// for (var i = 1; i < 89; i++) {
+//   console.log(i);
+// }
+
+// for (var i = 1; i < 100; i++) {
+//   console.log(i);
+
+//   if (i === 42) {
+//     break;
+//   }
+// }
+
+// for (var i = 1; i <= 89; i++) {
+//   if (i % 2 === 0) {
+//     continue;
+//   }
+//   console.log(i);
+// }
+
+for (var i = 1; i < 100; i++) {
+  if (i % 3 !== 0) {
     continue;
   }
   console.log(i);
--- M01S04/ex05/app.js
+++ M01S04/ex05/app.js
@@ -8,9 +8,22 @@
 //   console.log('acest numar este multiplu de 7');
 // }
 
-var number = prompt('introdu un numar:');
+// var number = prompt('introdu un numar:');
 
-for (var i = 5; i <= 100; i++) {
+// for (var i = 5; i <= 100; i++) {
+//   console.log(i);
+
+//   if (i % number !== 0) {
+//     continue;
+//   }
+
+//   console.log(`acest numar este multiplu de ${number}`);
+// }
+
+var number = prompt('introdu un numar:');
+var limit = prompt('introdu limita superioara:');
+
+for (var i = 5; i <= limit; i++) {
   console.log(i);
 
   if (i % number !== 0) {
