2044bfb85da78442247db7c5770d7eef93493726
RoxanaPin <roxana.pintilie28@gmail.com>
2022-08-10 22:08:30 +0300

    clasa

--- M01S06/ex04/app.js
+++ M01S06/ex04/app.js
@@ -1,23 +1,50 @@
 //siaf - iife
 
+// (function () {
+//   document.addEventListener('DOMContentLoaded', function () {
+//     const stage = document.querySelector('.stage');
+
+//     stage.addEventListener('mouseover', function () {
+//       console.log('Mouseul este pe scena');
+//       const p = document.createElement('p');
+//       p.innerText = 'Mouseul este pe scena';
+
+//       document.body.append(p);
+//     });
+
+//     stage.addEventListener('mouseout', function () {
+//       console.log('Mouseul NU este pe scena');
+//       const p = document.createElement('p');
+//       p.innerText = 'Mouseul NU este pe scena';
+
+//       document.body.append(p);
+//     });
+//   });
+// })();
+
 (function () {
   document.addEventListener('DOMContentLoaded', function () {
     const stage = document.querySelector('.stage');
 
     stage.addEventListener('mouseover', function () {
-      console.log('Mouseul este pe scena');
-      const p = document.createElement('p');
-      p.innerText = 'Mouseul este pe scena';
-
-      document.body.append(p);
+      const message = 'Mouseul este pe scena';
+      console.log(message);
+      showMessage(message);
     });
 
     stage.addEventListener('mouseout', function () {
-      console.log('Mouseul NU este pe scena');
-      const p = document.createElement('p');
-      p.innerText = 'Mouseul NU este pe scena';
-
-      document.body.append(p);
+      const message = 'Mouseul nu este pe scena';
+      console.log(message);
+      showMessage(message);
     });
   });
+
+  // hoisting
+  function showMessage(message) {
+    const paragraphElement = document.createElement('p');
+    paragraphElement.innerText = message;
+    paragraphElement.classList.add('message');
+
+    document.body.append(paragraphElement);
+  }
 })();
