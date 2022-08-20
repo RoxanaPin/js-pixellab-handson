33ec29259abf634623dba95bcbbe5268095e2ead
RoxanaPin <roxana.pintilie28@gmail.com>
2022-08-20 15:02:45 +0300

    n

---
+++ M01S01/node_intern/app.js
@@ -0,0 +1,24 @@
+// const greet = (name) => {
+//   console.log(`hey ${name}`);
+// };
+
+// greet('Roxana');
+// greet('Silviu');
+
+// (function () {
+//   foo();
+// })();
+
+// function foo() {
+//   console.log('foo called');
+//   bar();
+// }
+
+// var bar = function () {
+//   console.log('bar called');
+// };
+
+const array = [1, 2, 3];
+const array2 = [4, 5];
+const result = array.push(array2);
+console.log(result);
